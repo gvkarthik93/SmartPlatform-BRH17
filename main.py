3b4a0d002251d2ddc52b99bdd2fc34b5779e675f
Karthik <gvkarthik93@gmail.com>
2017-10-30 20:05:34 -0400

    Testing server

--- main.py
+++ main.py
@@ -5,10 +5,37 @@ import os
 PORT = int(os.environ.get('PORT', 8080))
 
 class myHandler(BaseHTTPRequestHandler):
+    #Handler for GET requests
     def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b"<html><body>Heroku is awesome</body></html>")
+        if self.path=="/":
+            self.path="/index.html"
+
+        if self.path=="/index":
+            self.path="/index.html"
+
+        try:
+            #Open the static file requested and send it
+            #Check the file extension required
+            #Set the right mime type
+            #rb is used to read all the mime types and display them
+            f = open(curdir + sep + self.path, 'rb')
+            mimetype, _ = mimetypes.guess_type(self.path)
+            try:
+                self.send_response(200)
+                self.send_header('Content-type',mimetype)
+                self.end_headers()
+                self.wfile.write(f.read())
+                f.close()
+            except:
+                print ("Connection Aborted: Established connection has been dropped")
+            return
+        except IOError:
+            self.send_error(404,'File Not Found: %s' % self.path)
+
+    #def do_GET(self):
+    #    self.send_response(200)
+    #    self.end_headers()
+    #    self.wfile.write(b"<html><body>Heroku is awesome</body></html>")
 
 class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     pass
